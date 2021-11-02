7b79bdf6d8b6a1ac91328e5589bfe0a5f66f4167
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-02 16:02:53 +0200

    increase by 5

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,33 @@
 import { createStore } from 'redux';
 
 const initialState = {
-	counter: 0
+	counter: 0,
+	showCounter: true
 };
 
 const counterReducer = (state = initialState, action) => {
 	if (action.type === 'increment') {
 		return {
+			...state,
 			counter: state.counter + 1
+		};
+	}
+	if (action.type === 'increase') {
+		return {
+			...state,
+			counter: state.counter + action.amount
 		};
 	}
 	if (action.type === 'decrement') {
 		return {
+			...state,
 			counter: state.counter - 1
+		};
+	}
+	if (action.type === 'toggle') {
+		return {
+			...state,
+			showCounter: !state.showCounter
 		};
 	}
 	return state;
